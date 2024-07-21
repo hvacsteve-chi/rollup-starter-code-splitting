61b2ae37f667f14dadfe4408c31023658788d0f3
Stephen <59994443+hvacsteve-chi@users.noreply.github.com>
2024-07-20 22:06:08 -0500

    changed

--- src/used-by-both.js
+++ src/used-by-both.js
@@ -1,3 +1,11 @@
+import { count, increment } from './incrementer.js';
+//import nodePolyfills from 'rollup-plugin-node-polyfills';
+
+console.log(count); // 0
+increment();
+console.log(count); // 1
+//count += 1; // Error - only incrementer.js can change this
+
 export default {
 	color: 'slategrey',
 	message: 'this is used by both entry points, so will be put in its own chunk'
